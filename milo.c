579b25461d3a0c4b46e0c899a016c8038dc3f52a
l0minous <zazadiler7@gmail.com>
2025-07-01 15:11:09 -0700

    raw mode

--- milo.c
+++ milo.c
@@ -1,3 +1,19 @@
-int main() {
+#include <termios.h>
+#include <unistd.h>
+
+void enableRawMode() {
+    struct termios raw;
+
+    tcgetattr( STDERR_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDERR_FILENO, TCSAFLUSH, &raw);
+}
+
+int main(void) {
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     return 0;
 }
